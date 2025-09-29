03f2b40aec5eb49ae5f823d1dfb8dec4119c61a4
fatimamachay-pixel <fatimamachay5@email.com>
2025-09-29 14:27:31 +0100

    modification data

--- src/data.py
+++ src/data.py
@@ -1,10 +1,14 @@
 import numpy as np
+stock = [
+    ["crayon", 100, 5],
+    ["Cahier", 50, 28.0],
+    ["Clé USB", 30, 15.0],
+    ["Gomme", 129, 1],
+    ["registre", 29, 55],
+    ["pc", 200, 3700],
+    ["tablette", 20, 1500]
+]
 
-nom_produit=[ "crayon", "cahier", "registre","cartable","pc","tablette" ]
-prix_unitaire=[12,34,50,200, 3600,1228]
-quantité= [110,56,50,24,3,45]
-arr1= np.array(nom_produit)
-# np.array([100, 200, 150])
-arr2= np.array(prix_unitaire)
-arr3= np.array(quantité)
-print(arr1,arr2,arr3)
+
+
+
