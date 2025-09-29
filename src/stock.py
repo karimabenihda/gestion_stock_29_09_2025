0b3846a0c2759130c54c03f14e846847b8ff1c17
karima <karimabenihda@gmail.com>
2025-09-29 12:57:44 +0100

    nothing

---
+++ src/stock.py
@@ -0,0 +1,13 @@
+from main import arr1,arr2,arr3
+
+added_product=input('entre product name: ')
+added_qte=int(input('entre product quantity: '))
+added_price=input('entre product price: ')
+
+arr1.append(added_product)
+arr2.append(added_qte)
+arr3.append(added_price)
+
+print(arr1)
+print(arr2)
+print(arr3)
