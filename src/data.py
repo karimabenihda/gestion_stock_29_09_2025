af4731ad8a45128c538491e0e4f897a59ef998e1
karima <karimabenihda@gmail.com>
2025-09-29 14:20:33 +0100

    functionalities

---
+++ src/data.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+nom_produit=[ "crayon", "cahier", "registre","cartable","pc","tablette" ]
+prix_unitaire=[12,34,50,200, 3600,1228]
+quantité= [110,56,50,24,3,45]
+arr1= np.array(nom_produit)
+arr2= np.array(prix_unitaire)
+arr3= np.array(quantité)
+print(arr1,arr2,arr3)
--- src/stock.py
+++ src/stock.py
@@ -1,13 +1,19 @@
-from main import arr1,arr2,arr3
+from data import arr1,arr2,arr3
+import numpy as np
 
 added_product=input('entre product name: ')
 added_qte=int(input('entre product quantity: '))
 added_price=input('entre product price: ')
 
-arr1.append(added_product)
-arr2.append(added_qte)
-arr3.append(added_price)
+arr1=np.append(arr1,added_product)
+arr2=np.append(arr2,added_qte)
+arr3=np.append(arr3,added_price)
 
+
+#supprimer un produits
+to_delete=input("entrer le nom produit que voulez supprimer: ")
+index = np.where(arr1 == to_delete)[0][0]
+
+np.delete(arr1,index)
 print(arr1)
-print(arr2)
-print(arr3)
+
