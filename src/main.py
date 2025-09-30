ab09373ef8ecbcf60e263a320def926a11e2b415
karima <karimabenihda@gmail.com>
2025-09-30 10:24:13 +0100

    giagrams matplotlib

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from data import stock
 from stock import ajouter,modifier,supprimer
+from visualize import pie,barres
 
 ajouter(stock)
 supprimer(stock)
 modifier(stock)
+barres(stock)
+pie(stock)
 
